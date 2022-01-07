e98747fbd8654eae067ea1117b06ad6aaede1a04
hien-alex <hiennnalex@gmail.com>
2022-01-07 00:14:17 -0800

    changed search

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:5000/api/v1/restaurants",
+  baseURL: "https://ahien-restaurant-reviews.herokuapp.com/api/v1/restaurants",
   headers: {
     "Content-type": "application/json",
   },
